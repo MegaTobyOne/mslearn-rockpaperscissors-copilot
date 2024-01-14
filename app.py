60416eebb414159a6a724723ccd23a4a5dd53ee9
TobyOne <24594009+MegaTobyOne@users.noreply.github.com>
2024-01-14 05:20:35 +0000

    used the solution branch, copilot knew exactly what to do ...

--- app.py
+++ app.py
@@ -1,18 +1,65 @@
 # write "Would you like to play a game?" to the console
 print("Would you like to play a game?")
 
-#start with an array of three choices, and a variable to hold the user's choice. The choices are rock, paper, or scissors.
+# import the random module
+import random
 
-choices = ["rock", "paper", "scissors"]
-user_choice = input("Choose rock, paper, or scissors: ")
+#create a list of options
+options = ["rock", "paper", "scissors"]
 
-# The computer should also randomly select one of the three options
+#create the score and round played variables
+score = 0
+rounds_played = 0
+
+#while loop to keep the game going
+while True:
+    #get the user's choice
+    user_choice = input("Choose rock, paper, or scissors: ")
+    #get the computer's choice
+    computer_choice = random.choice(options)
+    #print the computer's choice
+    print(f"Computer chose {computer_choice}")
+    #check for a tie
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    #check for rock
+    elif user_choice == "rock":
+        if computer_choice == "paper":
+            print("You lose!")
+            score -= 1
+        else:
+            print("You win!")
+            score += 1
+    #check for paper
+    elif user_choice == "paper":
+        if computer_choice == "scissors":
+            print("You lose!")
+            score -= 1
+        else:
+            print("You win!")
+            score += 1
+    #check for scissors
+    elif user_choice == "scissors":
+        if computer_choice == "rock":
+            print("You lose!")
+            score -= 1
+        else:
+            print("You win!")
+            score += 1
+    #print the score
+    print(f"Score: {score}")
+    #increment the rounds played
+    rounds_played += 1
+    #ask the user if they want to play again
+    play_again = input("Would you like to play again? (y/n) ")
+    #if the user doesn't want to play again, break out of the loop
+    if play_again != "y":
+        break
+#print the number of rounds played
+print(f"You played {rounds_played} rounds")
 
 
-# The computer should compare the player and computer's choice and determine the winner
-# The winner should be displayed to the user
-# The game should be replayed if the user chooses to play again
-# The game should end when the user chooses to quit
+
 
 
 
