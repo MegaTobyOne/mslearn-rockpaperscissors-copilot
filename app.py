05949b51b57b1d574a6e26fce9badbc1d5a4f8d3
TobyOne <24594009+MegaTobyOne@users.noreply.github.com>
2024-01-14 05:14:19 +0000

    no more messages

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-# write 'hi there' to the console
+# write "Would you like to play a game?" to the console
+print("Would you like to play a game?")
+
+#start with an array of three choices, and a variable to hold the user's choice. The choices are rock, paper, or scissors.
+
+choices = ["rock", "paper", "scissors"]
+user_choice = input("Choose rock, paper, or scissors: ")
+
+# The computer should also randomly select one of the three options
+
+
+# The computer should compare the player and computer's choice and determine the winner
+# The winner should be displayed to the user
+# The game should be replayed if the user chooses to play again
+# The game should end when the user chooses to quit
+
+
+
 
